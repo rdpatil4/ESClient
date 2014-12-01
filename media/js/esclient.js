074b3a58f799073bd350d0fba4fa0dba65ff5c47
rdpatil4 <rdpatil4@yahoo.com>
2014-12-01 15:15:54 -0500

    Autocomplete added and minor changes

--- media/js/esclient.js
+++ media/js/esclient.js
@@ -224,7 +224,7 @@ function getQueryResultsColumns()
 	var indxNameArr = $("#index").val();
 	// reset the header 
 	var columns = [];
-	var autoCompleteSource = [];
+	var autoCompleteSource = ["AND ", "OR ", "_exists_:", "_missing_:"];
 	var isTypeFound = false;
 	// add invisible column for the _id
 	columns.push({ "bVisible": true, "mData" : "_id", sTitle : "_id" });
@@ -393,7 +393,7 @@ function confirmDelete()
 	if (idToDelete)
 	{
 		$("#dialog").dialog('option', 'title', 'Please confirm!');
-		$("#dialog").text("Are you sure you want to delete rows with id: [" + idToDelete + "]" ).removeClass("ui-state-disabled");
+		$("#dialog").text("Are you sure you want to delete document(s) with id: [" + idToDelete + "]" ).removeClass("ui-state-disabled");
 		$( "#button-ok" ).removeClass("ui-state-disabled").attr("disabled", false);
 		$('#dialog').dialog("open");
 	}
@@ -551,16 +551,52 @@ function validateQueryAndSearch()
 function split( val ) {
   return val.split( /,\s*/ );
 }
-function extractLast( term ) {
-  return split( term ).pop();
-}
+    // Defines for the example the match to take which is any word (with Umlauts!!).
+    function _leftMatch(string, area) {
+        return string.substring(0, area.selectionStart).match(/[\wäöüÄÖÜß]+$/)
+    }
 
+    function _setCursorPosition(area, pos) {
+        if (area.setSelectionRange) {
+            area.setSelectionRange(pos, pos);
+        } else if (area.createTextRange) {
+            var range = area.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
+        }
+    }
 function setAutoCompleteForQuery()
 {
 	var colData = getQueryResultsColumns();
 	var formattedData;
-	$( "#query" ).autocomplete({
-      source: colData.autoCompleteSrc
+    $("#query").autocomplete({
+        position: { my : "right top", at: "right bottom" },
+        source: function(request, response) {
+            var str = _leftMatch(request.term, $("#query")[0]);
+            str = (str != null) ? str[0] : "";
+            response($.ui.autocomplete.filter(
+                    colData.autoCompleteSrc, str));
+        },
+        //minLength: 2,  // does have no effect, regexpression is used instead
+        focus: function() {
+            // prevent value inserted on focus
+            return false;
+        },
+        // Insert the match inside the ui element at the current position by replacing the matching substring
+        select: function(event, ui) {
+            var m = _leftMatch(this.value, this)[0];
+            var beg = this.value.substring(0, this.selectionStart - m.length);
+            this.value = beg + ui.item.value + this.value.substring(this.selectionStart, this.value.length);
+            var pos = beg.length + ui.item.value.length;
+            _setCursorPosition(this, pos);
+            return false;
+        },
+        search:function(event, ui) {
+            var m = _leftMatch(this.value, this);
+            return (m != null )
+        }
     });
 	return colData;
 }
@@ -602,7 +638,8 @@ function searchIndex()
 	   :{ "source": queryText , "sort": sortField, "from": $('#from').val(), "size": $('#size').val() , "lowercase_expanded_terms" : $('#lowerCaseExpandedTerms').is(':checked'), "analyze_wildcard" : $('#analyzeWildcard').is(':checked') , "search_type" : $('#searchType').val()[0] , "default_operator" : $('#defOperator').val()[0] }	
 	}).done(function ( results ) {
 		if ($('#showJsonResults').is(':checked')){
-			$('#jsonResults').html(prettifyJson(results.hits, $('#jsonResults'), true));
+			var updatedJson = updateJSONFields(results.hits.hits);
+			$('#jsonResults').html(prettifyJson(updatedJson, $('#jsonResults'), true));
 		}
 			formattedData = formatResultsData(results);
 			$("#totalResults").html('<strong> Total results found: [' + results.hits.total + ']</strong>');
@@ -747,13 +784,41 @@ function getTypeCssClass(value)
 		return retCssClsType;
 }
 
+function updateJSONFields(json)
+{
+		var selectFields = $("#indexFields").val();
+		if (typeof json != 'string')
+		{
+			 json = JSON.stringify(json, undefined, 4);
+		}
+		var jsonObj;
+		try
+		{ 
+			jsonObj = JSON.parse(json); 
+		}
+		catch(e)
+		{ 
+			alert('not valid JSON');
+			return;
+		}
+	
+	$.each(jsonObj, function(index,jsonRow){
+		$.each(jsonRow._source, function(jsonFieldName,jsonFieldVal){
+			if (selectFields.indexOf(jsonFieldName) < 0)
+			{
+				delete jsonObj[index]._source[jsonFieldName];
+			}
+		});
+	});
+	return jsonObj;
+}
 
 function prettifyJson(json, divId, isExpandAll)
 {
-    if (typeof json != 'string')
-	{
-         json = JSON.stringify(json, undefined, 4);
-    }
+		if (typeof json != 'string')
+		{
+			 json = JSON.stringify(json, undefined, 4);
+		}
 		var jsonObj;
 		try
 		{ 
